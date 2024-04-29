608402f03c85c39a3b72d6849c436a6e6352c580
David Leach <david.leach@nxp.com>
2024-04-29 14:55:50 -0500

    Added "TomasGalbickaNXP"
    Signed-off-by: David Leach <david.leach@nxp.com>

--- nxp_pr_queue.py
+++ nxp_pr_queue.py
@@ -97,7 +97,7 @@ class NXP_Zephyr:
             "lylezhu2012", "vakulgarg", "Ursescu", "laurenpost",
             "agansari", "Lucien-Zhao", "NeilChen93", "ChayGuo",
             "sumitbatra-nxp", "PetervdPerk-NXP", "bperseghetti", 
-            "igalloway", "mayankmahajan-nxp"]
+            "igalloway", "mayankmahajan-nxp", "TomasGalbickaNXP"]
 
         print(f"NXP_Zephyr: {self.NXP_Zephyr_Team}")
 
